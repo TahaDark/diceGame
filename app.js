b4731417e3ad01b60c2ef4ef4f576a3944691261
Taha <tahamojtabavidev@gmail.com>
2023-01-06 17:16:36 +0330

    design, logics, variables changed

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const btnEl = document.querySelectorAll(".roll-btn");
+const btnEl = document.querySelector(".roll-btn");
 const scoreEl0 = document.querySelector(".score-0");
 const scoreEl1 = document.querySelector(".score-1");
 const diceEl0 = document.querySelector(".dice-0");
 const diceEl1 = document.querySelector(".dice-1");
-let scores, activePlayer;
+let scores, activePlayer, dice0, dice1;
 
 const init = function () {
   scores = [0, 0];
-  btnEl[0].classList.remove(hide);
-  btnEl[1].classList.add(hide);
-  diceEl0[0].classList.add(hide);
-  diceEl1[0].classList.add(hide);
   activePlayer = 0;
+};
+const switchPlayer = function () {
+  if (activePlayer === 0) {
+    document.querySelector(".btn-box").style.left = "50%";
+    activePlayer = 1;
+  } else {
+    document.querySelector(".btn-box").style.left = "0";
+    activePlayer = 0;
+  }
 };
 
 init();
+
+btnEl.addEventListener("click", function () {
+  dice0 = Math.trunc(Math.random() * 6) + 1;
+  document.querySelector(`.dice-${activePlayer}`).classList.remove("hide");
+  document.querySelector(`.dice-${activePlayer}`).src = `img/dice${dice0}.png`;
+  switchPlayer();
+});
